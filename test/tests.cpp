eb76abeeb9c1ec3388a7d5e58a8fd9048e599bb8
anvar <mr.nazyrov1997@mail.ru>
2026-06-08 21:25:09 +0300

    ST-2

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,9 +1,9 @@
 // Copyright 2025 UNN-CS
 // Nazyrov A.A.
+
+#include "circle.h"
 #include "tasks.h"
 #include <gtest/gtest.h>
-#include "circle.h"
-
 
 TEST(MyCircleTest, ConstructorWithRadius) {
     MyCircle fig(5.0);
